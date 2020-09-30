aa8d7ed050c5c96cbdd79758a933bd66f5d4530d
Spartaco Amadei <spamadei@gmail.com>
2020-10-01 01:26:36 +0200

    pass to rr conf
    Change-Id: Ife80465c1f1f5023333887e09d059f788c238365

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,9 +15,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_beyond0lte.mk
+    $(LOCAL_DIR)/rr_beyond0lte.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_beyond0lte-eng \
-    lineage_beyond0lte-user \
-    lineage_beyond0lte-userdebug
+    rr_beyond0lte-eng \
+    rr_beyond0lte-user \
+    rr_beyond0lte-userdebug
--- rr_beyond0lte.mk
+++ rr_beyond0lte.mk
@@ -15,7 +15,7 @@ TARGET_SCREEN_WIDTH := 1080
 TARGET_BOOTANIMATION_HALF_RES := true
 
 ### LINEAGE
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/rr/config/common_full_phone.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_NAME := lineage_beyond0lte
